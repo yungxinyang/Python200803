d59406f4917bfe0eeba07020767f8073f2285ca0
yungxinyang <69131715+yungxinyang@users.noreply.github.com>
2020-08-03 11:56:27 +0800

    Add files via upload

---
+++ day1-3.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Aug  3 10:36:14 2020
+
+@author: user
+"""
+圓的半徑=input("圓的半徑")
+圓的半徑=float(圓的半徑)
+print('圓周長',圓的半徑*2*3.14)
+
+圓的半徑=input("圓的半徑")
+圓的半徑=float(圓的半徑)
+print("圓面積",圓的半徑**2*3.14)
