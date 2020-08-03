64b6596e452d29e258c6abc8e600e3c6bf7033f5
yungxinyang <69131715+yungxinyang@users.noreply.github.com>
2020-08-03 15:54:54 +0800

    Add files via upload

--- day1.1.py
+++ day1.1.py
@@ -8,7 +8,7 @@ Created on Mon Aug  3 14:56:25 2020
 height=float(input('hieght'))/100
 weight=float(input('weight'))
 BMI=weight//height**2
-print('BMI')
+print('BMI',weight//height**2)
 if BMI<18.5:
     print('體重不足')
 elif BMI<24:
@@ -21,4 +21,5 @@ elif BMI<35:
     print('中度肥胖')
 else:
     print('重度肥胖')
+
 
