10659f1cd038b6d159e75d6d6e677f0665b7c0b6
yungxinyang <69131715+yungxinyang@users.noreply.github.com>
2020-08-03 11:18:17 +0800

    Add files via upload

---
+++ day1-1.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Aug  3 10:05:58 2020
+
+@author: user
+"""
+print("i'm Eric \nHe is'cool'")
+print("i'm Eric")
+print('He is "cool"')
---
+++ day1-2.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Aug  3 10:27:30 2020
+
+@author: user
+"""
+
+name=input("what's your name?")
+print("Hi",name)
