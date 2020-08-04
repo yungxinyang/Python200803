81a0e26bb4b70322bcc68b391854b018ab39dbe7
yungxinyang <69131715+yungxinyang@users.noreply.github.com>
2020-08-04 10:50:53 +0800

    Add files via upload

---
+++ Day2-2.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Aug  4 10:20:37 2020
+
+@author: user
+"""
+
+import random
+n = random.randint(1,20)
+while True:
+        number=input("number")
+        number=int(number)
+        if number>20 or number<0:
+            print('輸入錯誤')
+        else:
+            if number>n:
+                print('再小一點')
+            elif number<n:
+                print('再大一點')
+            else:
+                print('BINGO')
+                break 
+        i=0
+        i=i+1
+        if i>5:
+            break
